11616968723c55fd0595ca1c7961345e5c1a3ebe
Save <save9253@mail.ru>
2021-03-15 20:59:11 -0400

    changed to the face version

---
+++ src/components/styles.js
@@ -0,0 +1,28 @@
+const colors = {
+    ltGreen:'#c3e06d',
+    mdGray:'#36343c',
+    mdGreen:'#7c8a54',
+    drGreen:'#515a38',
+    black:'#1a1a1a',
+    red:'#b25c59',
+    ltRed: '#eb7f7b',
+    drRed:'#9a3d3a',
+    white50:'rgba(255,255,255,0.5)'
+}
+
+const styles = {
+    linkDiv:{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 5,
+        flexWrap: 'wrap'
+    },
+    link:{
+        padding: '8px 10px',
+        borderRadius: '2px',
+        fontSize: '20px',
+        fontFamily: '"Do Hyeon", sans-serif'
+    }
+}
+export {colors,styles}
